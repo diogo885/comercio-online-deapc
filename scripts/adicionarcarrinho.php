3744089ce54b54553eb800dd77e668b86cebbf7e
diogo885 <diogoomartins116@gmail.com>
2026-05-29 12:22:23 +0000

    g2

---
+++ scripts/adicionarcarrinho.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "<h1>Carrinho</h1>";
+
+$produto = $_POST["produto"];
+$quantidade = $_POST["quantidade"];
+
+echo "Produto: " . $produto . "<br>";
+echo "Quantidade: " . $quantidade . "<br>";
+
+?>
--- scripts/novoregisto.php
+++ scripts/novoregisto.php
@@ -1,10 +1,12 @@
 <?php
 
-echo "<h1>Teste Login</h1>";
+echo "<h1>Teste de Registo</h1>";
 
+$nome = $_POST["nome"];
 $email = $_POST["email"];
 $password = $_POST["password"];
 
+echo "Nome: " . $nome . "<br>";
 echo "Email: " . $email . "<br>";
 echo "Password: " . $password . "<br>";
 
