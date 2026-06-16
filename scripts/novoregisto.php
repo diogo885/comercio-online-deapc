aa4134609fc8353e43dc15c143b6d975acbfb5bc
diogo885 <diogoomartins116@gmail.com>
2026-06-16 16:12:08 +0000

    Implementação do registo de utilizadores

--- scripts/novoregisto.php
+++ scripts/novoregisto.php
@@ -1,5 +1,7 @@
 <?php
 
+$db = new SQLite3('../database/loja.db');
+
 echo "<h1>Teste de Registo</h1>";
 
 $nome = $_POST["nome"];
@@ -9,5 +11,14 @@ $password = $_POST["password"];
 echo "Nome: " . $nome . "<br>";
 echo "Email: " . $email . "<br>";
 echo "Password: " . $password . "<br>";
+
+$sql = "INSERT INTO utilizadores (nome, email, password)
+        VALUES ('$nome', '$email', '$password')";
+
+if ($db->exec($sql)) {
+    echo "<br><strong>Utilizador registado com sucesso!</strong>";
+} else {
+    echo "<br><strong>Erro ao registar utilizador.</strong>";
+}
 
 ?>
